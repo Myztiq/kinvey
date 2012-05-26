b50e16d89f475eae2fb0aca1c640a312e6c4a6de
Myztiq <ryan@ryan-kahn.com>
2012-05-26 16:39:43 -0400

    Added sort and display only active button

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -6,6 +6,7 @@
     self.initialized = ko.booleanObservable(false);
     self.todoList = ko.observableArray([]);
     self.newListItemText = ko.observable();
+    self.showCompleted = ko.booleanObservable(true);
 
     //METHODS
     //METHODS
@@ -24,6 +25,16 @@
       listItem.remove();
     }
 
+    self.sort = function(){
+      self.todoList.sort(function(a,b){
+        var key= 'title';
+        var leftKey = a[key]().toLowerCase();
+        var rightKey = b[key]().toLowerCase();
+        return  leftKey == rightKey ? 0 : (leftKey < rightKey ? -1 : 1);
+      });
+    }
+
+
     //INITIALIZATION
     //INITIALIZATION
     //INITIALIZATION
@@ -37,6 +48,7 @@
         for(var i=0;i<list.length;i++){
           self.todoList.push(new ListItem(list[i]));
         }
+        self.sort();
       },
       error: function(error) {
         alert(error.error);
@@ -46,36 +58,33 @@
     return this;
   }
 
+  /**
+   * Custom knockout extender to save the entire list item on change
+   * @param target
+   * @param options
+   */
+  ko.extenders.saveOnChange = function(target, options) {
+    target.subscribe(function(newVal){
+      if(newVal){
+        options.kEntity.set(options.key,newVal);
+        options.kEntity.save();
+      }
+    });
+    return target;
+  };
+
+
+
   function ListItem(kEntity){
     var self = this;
-    self.title = ko.observable('');
-    self.title(kEntity.get('title'));
-    self.title.subscribe(function(newValue){
-      kEntity.set('title',newValue);
-      kEntity.save({
-        error: function(error){
-          alert(error.error);
-        }
-      });
-    });
-
-    self.completed = ko.booleanObservable(false);
-    self.completed(kEntity.get('completed'));
-    self.completed.subscribe(function(newValue){
-      kEntity.set('completed',newValue);
-      kEntity.save({
-        error: function(error){
-          alert(error.error);
-        }
-      });
-    });
+    self.title = ko.observable(kEntity.get('title')).extend({saveOnChange:{key: 'title', kEntity: kEntity}});
+    self.completed = ko.booleanObservable(kEntity.get('completed')).extend({saveOnChange:{key: 'completed', kEntity: kEntity}});
 
     //METHODS
     //METHODS
     //METHODS
     //METHODS
     //METHODS
-
     self.remove = function(){
       kEntity.destroy();
     }
